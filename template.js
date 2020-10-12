9edfec7757c8312902d506c711602b3bcb657b5b
bmamran <bm.amran.bd@gmail.com>
2020-10-12 23:13:21 +0600

    added

---
+++ template.js
@@ -0,0 +1,10 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName;
+const fullName2 = `${firstName} ${12+37+57+91} is a good boy`;
+const multiLine = `I'm Baktear Mohammed Amran
+I was born in Fatikchari
+Now, I live in Dhaka
+Dhaka is the capital of Bangladesh
+Bangladesh is my Motherland`
+console.log(multiLine);
